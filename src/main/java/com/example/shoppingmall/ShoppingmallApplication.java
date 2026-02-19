94be2164d52c90954b8e59cf3b99dd81411b6cf9
Woori <bini219@naver.com>
2026-02-19 14:42:31 +0900

    초기 설정

--- src/main/java/com/example/shoppingmall/ShoppingmallApplication.java
+++ src/main/java/com/example/shoppingmall/ShoppingmallApplication.java
@@ -2,8 +2,10 @@ package com.example.shoppingmall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing  //날짜 자동생성
 public class ShoppingmallApplication {
 
     public static void main(String[] args) {
