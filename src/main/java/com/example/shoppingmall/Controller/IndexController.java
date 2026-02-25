05cac1cb6d7449d396d49c2ae9e4f377edd7fa61
Woori <bini219@naver.com>
2026-02-25 17:41:56 +0900

    IndexController 2

--- src/main/java/com/example/shoppingmall/Controller/IndexController.java
+++ src/main/java/com/example/shoppingmall/Controller/IndexController.java
@@ -3,8 +3,12 @@ package com.example.shoppingmall.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class IndexController {
+    private static final List<String> CATEGORIES =
+            List.of("전체", "교육", "음악", "게임", "여행", "요리", "스포츠", "엔터테인먼트", "뉴스", "기타");
     @GetMapping
     public String index() {
         return "index";
