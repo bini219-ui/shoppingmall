5a5ec5fb65917a0ba557e1b309fac2c81b20fb53
Woori <bini219@naver.com>
2026-02-26 14:38:08 +0900

    완성

--- src/main/java/com/example/shoppingmall/Controller/IndexController.java
+++ src/main/java/com/example/shoppingmall/Controller/IndexController.java
@@ -7,8 +7,6 @@ import java.util.List;
 
 @Controller
 public class IndexController {
-    private static final List<String> CATEGORIES =
-            List.of("전체", "교육", "음악", "게임", "여행", "요리", "스포츠", "엔터테인먼트", "뉴스", "기타");
     @GetMapping
     public String index() {
         return "index";
